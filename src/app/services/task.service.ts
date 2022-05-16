2e12d06c0e5b58100bf7287775b815727147ac3e
Viktoriia <krjuchenkova@gmail.com>
2022-05-17 00:48:05 +0300

    get all tasks service added

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { TaskResponse } from '../common/interfaces/response';
 import { TaskItem } from '../common/interfaces/task';
 
 @Injectable({
@@ -14,6 +15,21 @@ export class TaskService {
   ) { }
 
   addTask(task: TaskItem): Observable<TaskItem> {
-    return this.http.post<TaskItem>(`${environment.dbUrl}/tasks/planned.json`, task)
+    return this.http.post<TaskItem>(`${environment.dbUrl}/tasks.json`, task)
   }
+
+  getAll(): Observable<any> {
+    return this.http.get<any>(`${environment.dbUrl}/tasks.json`)
+    .pipe(
+      map(res => {
+       return Object
+       .keys(res)
+       .map(el => {
+         return {
+           ...res[el]
+         }
+       })
+      })
+    )
+    }
 }
