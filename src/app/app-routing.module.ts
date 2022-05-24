5a1107affc35c1b0b5221aeae71abb66a1fcc28e
Viktoriia <krjuchenkova@gmail.com>
2022-05-24 12:14:50 +0300

    header changes, buttons and routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { MainViewComponent } from './pages/main-view/main-view.component';
 
 const routes: Routes = [ 
+  {
+    path: "", component: HomepageComponent
+  },
+  {
+    path: "dashboard", component: MainViewComponent
+  }
 ];
 
 @NgModule({
